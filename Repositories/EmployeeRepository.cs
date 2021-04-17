8d322105f8421dc38264bba2adff5546f0ee6dcd
Yimura <andreas.maerten@scarlet.be>
2021-04-17 22:30:30 +0200

    feat(Repo): Added pagination and GetEmployee

--- Repositories/EmployeeRepository.cs
+++ Repositories/EmployeeRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using API.IMDB.DataContext;
 using API.IMDB.Models;
@@ -8,7 +9,8 @@ namespace API.IMDB.Repositories
 {
     public interface IEmployeeRepository
     {
-        Task<ICollection<Employee>> GetEmployees();
+        Task<ICollection<Employee>> GetEmployees(int page, int limit);
+        Task<Employee> GetEmployee(int id);
     }
 
     public class EmployeeRepository : IEmployeeRepository
@@ -20,9 +22,16 @@ namespace API.IMDB.Repositories
             this._context = context;
         }
 
-        public async Task<ICollection<Employee>> GetEmployees()
+        public async Task<Employee> GetEmployee(int id)
         {
-            return await this._context.Employees.ToListAsync();
+            return await this._context.Employees.Where(e => e.EmployeeId == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<ICollection<Employee>> GetEmployees(int page, int limit)
+        {
+            int skip = limit * (page - 1);
+
+            return await this._context.Employees.Skip(skip).Take(limit * page - skip).ToListAsync();
         }
     }
 }
