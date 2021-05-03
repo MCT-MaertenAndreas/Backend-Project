053661af9413abdbe66ba5fb40dd2cb7003e32b9
Yimura <andreas.maerten@scarlet.be>
2021-05-03 11:23:59 +0200

    feat(App): Disable HTTPS
    The backend should be proxied through a web server that has HTTPS.

--- API.Employees/Startup.cs
+++ API.Employees/Startup.cs
@@ -70,7 +70,7 @@ namespace API.Employees
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", Program.Title + " v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
